76a0116b6310fd5e67dd7c294bcbc3b99b77fe20
DELL <mr2079@fayoum.edu.eg>
2024-12-23 13:06:07 -0600

    add space

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         backgroundColor: Colors.cyan,
         title: Text("islami"),
